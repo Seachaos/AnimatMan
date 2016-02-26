90ed2bb5a5c7bfca8bd222db28e9c112d71a9401
Seachaos <ye.jian.yin@gmail.com>
2016-02-27 00:08:33 +0800

    let image can be merge by gulp

--- TestSite/gulpfile.js
+++ TestSite/gulpfile.js
@@ -4,6 +4,20 @@ var webserver = require('gulp-webserver');
 // https://github.com/gobwas/gulp-sprite-generator
 var sprite = require('gulp-sprite-generator');
 
+var compass   = require('gulp-compass');
+
+
+gulp.task('compass',function(){
+    gulp.src('./css_src/*.scss')
+        .pipe(compass({
+            sourcemap: true,
+            time: true,
+      css: './css_src/',
+      sass: './css_src/',
+      style: 'compact' //nested, expanded, compact, compressed
+        }))
+        .pipe(gulp.dest('./app/css/'));
+}); 
 
 gulp.task('sprites', function() {
     var spriteOutput;
@@ -12,7 +26,7 @@ gulp.task('sprites', function() {
         .pipe(sprite({
             baseUrl: "./images",
             spriteSheetName: "sprite.png",
-            spriteSheetPath: "./images",
+            spriteSheetPath: "./images"
         }));
 
     spriteOutput.css.pipe(gulp.dest("./app/css"));
@@ -33,5 +47,8 @@ gulp.task('webserver', function() {
 gulp.watch('./css_src/**.css', function() {
     gulp.run('sprites');
 });
+gulp.watch('./css_src/**.scss', function() {
+    gulp.run('compass');
+});
 
-gulp.task('default',['sprites', 'webserver']);
+gulp.task('default',['compass', 'sprites', 'webserver']);
